a0d6119b4f3c575d51c13eb92381adabbf91ef94
deb <35792579+DebHarrington@users.noreply.github.com>
2020-01-14 15:16:41 +1100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,9 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvYL/VbLATT7wYo59WpdflftVm63tSl+kgPIvUeblXCF8J/wC7vJDObwXYprZZVJWtCAYEgBCrAOBfC2Xw
+zNHTKCLCBKNlQqnGwcvYUstAZvzL3lnki7urRoHc+Halq9oqCch/hXMCkbxZp4MR+6dIRfJUQLTGG9k8x5Pc3nXGpaa4ORjo5bNZZfpey9lqajjwDkGi/70Z
+bftflNAdCrsITYIzgBaE/DDLEw1mFDuoZHtJzbfvlldQI0Y4CHvg9cKIivAzRhgbkmzNtD/Ca23xo4RfCsarsVlEtIEsMbsdLbj2JAOTVp2f0LnCeRLFDRuv
+vi5L7bcP14vhBqdzol1D root@master.puppet.vm',
 	}  
 }
